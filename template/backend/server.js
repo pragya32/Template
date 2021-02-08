ef3c3fe48fd9c3bbcaf8eaaf5334d3fa47d2bc5f
Pragya Shukla <shuklapragya495@gmail.com>
2021-02-08 14:53:47 +0530

    3 one

--- template/backend/server.js
+++ template/backend/server.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const router = require("./routers");
+const cors = require("cors");
+require("dotenv").config();
+mongoose.connect(process.env.DATABASE, () => console.log("connected"));
+app.use(express.json());
+app.use(cors());
+app.use("/app", router);
 app.listen(4000, () => console.log("listening"));
